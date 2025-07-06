76d1d7b32b4084e71153252ebae2e525eb69036c
IsaacOrtga <ortga.isaac@gmail.com>
2025-07-06 20:16:50 +0200

    Last changes with academiesList

---
+++ src/components/ReedemButton.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material"
+
+const ReedemButton = () => {
+  return (
+    <>
+        <Button variant="outlined">Tengo mi bono</Button>
+    </>
+  )
+}
+
+export default ReedemButton
