da5fd611a329cc376845595d3519f4367019a69e
IsaacOrtga <ortga.isaac@gmail.com>
2025-07-13 19:40:19 +0200

    Home: desktop view finished

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 import FormComponent from "../components/FormComponent";
-import ReedemButton from "../components/ReedemButton";
+import RedeemButton from "../components/RedeemButton";
 import type { AcademyType } from "../types/academy_type";
 import { getAcademies } from "../api/get";
 import BannerImg from "../assets/img/banner_nobg.png";
@@ -33,24 +35,39 @@ const Home = () => {
   };
 
   return (
-    <div>
-      <div className="w-100 flex justify-end mt-5">
-        <ReedemButton />
+    <div className="homeContainer">
+      <div className="mainHomeContainer flex flex-col">
+        <div className="redeemButtonMobile w-100 flex justify-end mt-5">
+          <RedeemButton />
+        </div>
+        <div className="flex flex-col items-center justify-center mt-10">
+          <Logo />
+          <h5>Compra y canjea</h5>
+        </div>
+        <div className="flex justify-center">
+          <img src={BannerImg} alt="Banner Bondl" />
+        </div>
       </div>
-      <div className="flex flex-col items-center justify-center mt-10">
-        <Logo />
-        <h5>Compra y canjea</h5>
-      </div>
-      <div className="flex justify-center">
-        <img src={BannerImg} alt="Banner Bondl" />
-      </div>
-      <div className="p-7 flex flex-col gap-5">
-        <FormComponent
-          selectedAcademy={selectedAcademy}
-          setSelectedAcademy={setSelectedAcademy}
-          academiesList={academiesList}
-          sendInformation={sendInformation}
-        />
+      <div className="formHomeContainer p-7 flex flex-col gap-5">
+        <Card variant="outlined" className="redeemButtonDesktop">
+          <h5 className="text-xl font-bold">
+            Si ya tienes un bono, canjéalo en el siguiente paso
+          </h5>
+          <div>
+            <RedeemButton />
+          </div>
+        </Card>
+        <Card variant="outlined" className="formHomeContent flex flex-col gap-5">
+          <h5 className="formHomeSubtitle text-xl font-bold">
+            O compra un nuevo bono
+          </h5>
+          <FormComponent
+            selectedAcademy={selectedAcademy}
+            setSelectedAcademy={setSelectedAcademy}
+            academiesList={academiesList}
+            sendInformation={sendInformation}
+          />
+        </Card>
       </div>
     </div>
   );
